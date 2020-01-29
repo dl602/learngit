dc7dab349133bc0c11b59b30f7811a8f67c17beb
unknown <dl602@live.mdx.ac.uk>
2020-01-29 19:02:59 +0000

    index.html added

---
+++ server.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var router = express.Router();
+var path = require('path');
+const app = express();
+
+
+
+// load bodyParser module for json payload parsing
+
+
+var urlencodeParser = bodyParser.urlencoded({extended:true})
+app.use(bodyParser.json())
+
+
+// connect to MongoDB
+const MongoClient = require('mongodb').MongoClient;
+let db;
+      MongoClient.connect('mongodb://127.0.0.1:27017/', (err, client) => {
+db = client.db('mydatabase')
+})
+
+
+// get the collection name
+app.param('collectionName', (req, res, next, collectionName) => {
+req.collection = db.collection(collectionName)
+console.log('collection name:', req.collection)
+return next()
+})
+
+// dispaly a message for root path to show that API is working
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+})
+// retrieve all the objects from an collection
+app.get('/collections/:collectionName',  (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+req.collection.find({}, { limit: 10, sort: [['price', -1]] }).toArray((e, results) => {
+if (e) return next(e)
+res.send(results)
+})
+})
+// add a lesson
+app.post('/collections/:collectionName',urlencodeParser, (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+// TODO: Validate req.body
+
+req.collection.insertOne(req.body, (e, results) => {
+if (e) return next(e)
+res.send(results.ops)
+console.log(req.body)
+})
+})
+//login
+app.post('/login/collections/:collectionName',urlencodeParser, (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+// TODO: Validate req.body
+var email = req.body.email
+req.collection.findOne({ email: req.body.email}, function(err, user) {
+    if(user ===null){
+      res.send('not registered');
+     
+   }else if (user.email === req.body.email && user.password === req.body.password){
+   res.send('successful');
+   console.log('successful')
+   
+ } else {
+   console.log("Credentials wrong");
+   res.end("credentials wrong");
+ }
+});
+
+
+
+
+}
+
+)
+// retrieve a lesson by mongodb ID
+const ObjectID = require('mongodb').ObjectID;
+
+app.get('/collections/:collectionName/:id', urlencodeParser, (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+console.log('searching json object with id:', req.params.id)
+req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+if (e) return next(e)
+res.send(result)
+})
+})
+// update a lesson by ID
+app.put('/collections/:collectionName/:id', urlencodeParser, (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+req.collection.update({ _id: new ObjectID(req.params.id) },
+{ $set: req.body },
+{ safe: true, multi: false }, (e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
+})
+})
+// delete a lesson by ID
+app.delete('/collections/:collectionName/:id', (req, res, next) => {
+req.collection.deleteOne({ _id: ObjectID(req.params.id) }, (e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
+})
+})
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+})
+
