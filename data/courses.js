f80f6adc3c32e7126d1d75bb27da0e3cb3f35549
unknown <dl602@live.mdx.ac.uk>
2020-02-02 23:10:00 +0000

    data file

---
+++ data/courses.js
@@ -0,0 +1,89 @@
+var courses = [{
+    slug: 'science',
+    name: 'science',
+    author: 'Zosia and Bartek',
+    twitter: 'bartaz',
+    website: '',
+    github: 'github.com/bartaz/lost-in-cyberspace'
+},
+{
+    slug: 'dance',
+    name: 'dance',
+    author: 'Platane',
+    twitter: 'platane_',
+    website: 'github.com/Platane',
+    github: 'github.com/Platane/js13k-2017'
+},
+{
+    slug: 'dart',
+    name: 'dart',
+    author: 'Mary Knize',
+    twitter: 'captainpainway',
+    website: 'maryknize.com',
+    github: 'github.com/captainpainway/coconutty'
+},
+{
+    slug: 'math',
+    name: 'math',
+    author: 'MarcGuinea',
+    twitter: 'MarcGuineaCasas',
+    website: 'marcguinea.com',
+    github: 'github.com/mguinea/lost-pacman'
+},
+{
+    slug: 'swimming',
+    name: 'swimming',
+    author: 'Alex Swan',
+    twitter: 'BoldBigflank',
+    website: 'bold-it.com',
+    github: 'github.com/BoldBigflank/js13k-polyhedron'
+},
+{
+    slug: 'english',
+    name: 'English',
+    Tutor: 'Dan',
+    twitter: 'madmarcel',
+    website: '',
+    github: 'github.com/madmarcel/js13k2017'
+},
+{
+    slug: 'french',
+    name: 'french',
+    author: 'Sorskoot',
+    twitter: 'Sorskoot',
+    website: 'timmykokke.com',
+    github: 'github.com/sorskoot/js13kgames_2017_Lost'
+},
+{
+    slug: 'piano',
+    name: 'piano',
+    author: 'Nylki',
+    twitter: 'nylk',
+    website: 'github.com/nylki',
+    github: 'github.com/nylki/shifted-dimensions'
+},
+{
+    slug: 'guitar',
+    name: 'guitar',
+    author: 'Jack Greenberg',
+    twitter: 'thprgrmmrjck',
+    website: '',
+    github: 'github.com/theProgrammerJack/js13k2017'
+},
+{
+    slug: 'boxing',
+    name: 'boxing',
+    author: 'Kenneth Banico',
+    twitter: 'kjdesigns671',
+    website: '',
+    github: 'github.com/kbanico/lost-in-guam-vr-game'
+},
+{
+    slug: 'cooking',
+    name: 'cooking',
+    author: 'Fasility',
+    twitter: 'Fasility_VR',
+    website: 'fasility.com',
+    github: 'github.com/flowerio/balloon-problems'
+},
+]
