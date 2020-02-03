eee85c8680506d356a3d5e8d1330fda895994020
unknown <dl602@live.mdx.ac.uk>
2020-02-03 18:40:34 +0000

    m

--- app.js
+++ app.js
@@ -245,20 +245,4 @@ imagesToLoad.forEach((img) => {
     loadImages(img);
     });
     
-    if('IntersectionObserver' in window) {
-        const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-        if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-        }
-        });
-        });
-        imagesToLoad.forEach((img) => {
-        observer.observe(img);
-        });
-        } else {
-        imagesToLoad.forEach((img) => {
-        loadImages(img);
-        });
-        }
+    
