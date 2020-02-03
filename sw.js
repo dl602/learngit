3692c997642ff702f1681d58b83414e6b560e3a2
unknown <dl602@live.mdx.ac.uk>
2020-02-03 07:43:57 +0000

    index

--- sw.js
+++ sw.js
@@ -3,18 +3,18 @@ self.addEventListener('install', (e) => {
 });
 
 var cacheName = 'cw2-1';
+var coursesImages = [];
+for(var i=0; i<courses.length; i++) {
+CoursesImages.push('data/img/'+courses[i].slug+'.jpg');
+}
+var contentToCache = appShellFiles.concat(coursesImages);
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll(
-          [
-            '/pwa/data',
-            '/pwa/data/img/math.jpg',
-            'pwa/data/img/science.jpg',
-            'pwa/index.html'
-          ]
-        );
-      })
-    );
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+  caches.open(cacheName).then((cache) => {
+  console.log('[Service Worker] Caching all: app shell and content');
+  return cache.addAll(contentToCache);
+  })
+  );
   });
